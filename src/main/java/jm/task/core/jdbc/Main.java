11c5bdc38bc11b268fcd179adebcfdb70e1190a5
Okami <mr.naydyuk2@gmail.com>
2025-02-21 19:49:07 +0800

    fix problems

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,20 +12,19 @@ public class Main {
     public static void main(String args[]) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        User user1 = new User("ivan", "ivanov", (byte) 30);
-        userService.saveUser(user1.getName(), user1.getLastName(), user1.getAge());
-        User user2 = new User("petr", "petrov", (byte) 40);
-        userService.saveUser(user2.getName(), user2.getLastName(), user2.getAge());
-        User user3 = new User("sveta", "svetova", (byte) 50);
-        userService.saveUser(user3.getName(), user3.getLastName(), user3.getAge());
-        User user4 = new User("kata", "katova", (byte) 60);
-        userService.saveUser(user4.getName(), user4.getLastName(), user4.getAge());
-        List<User> users = new ArrayList<>(userService.getAllUsers());
-        for (User user : users) {
+
+        userService.saveUser ("Sasha", "Smirnov", (byte) 13);
+        userService.saveUser ("Ivan", "Stalin", (byte) 31);
+        userService.saveUser ("Nikita", "Ponomaryev", (byte) 39);
+        userService.saveUser ("Natasha", "Tumanova", (byte) 20);
+
+        List<User> users = userService.getAllUsers();
+        System.out.println("Все пользователи из базы данных:");
+        for (User  user : users) {
             System.out.println(user);
         }
         userService.cleanUsersTable();
-        userService.getAllUsers();
+        userService.dropUsersTable();
     }
 
 }
